f6b21f4f747c59d6330ee1ca75c0848fa3b3fb47
= <fernandonvieira47@hotmail.com>
2020-07-17 13:13:40 -0300

    Updates

---
+++ precache-manifest.9f9c3b3683825b22ae8249964b5a8acd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ca8d470f8679ee1818536022f92171",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "09cb8e6d1bcc8930c3cd",
+    "url": "/monsters-rolodex/static/css/main.b72013f0.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09cb8e6d1bcc8930c3cd",
+    "url": "/monsters-rolodex/static/js/main.372db30f.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
